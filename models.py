fc13d6991ef0b4bda720fb4df7bbb72b0475c3e4
MANISHGHGH <95271062+MANISHGHGH@users.noreply.github.com>
2024-10-30 17:08:20 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,12 +1,32 @@
-from pydantic import BaseModel, EmailStr
-from typing import Optional
+from pydantic import BaseModel
+from typing import List, Optional
 
-class User(BaseModel):
-    username: str
-    email: EmailStr
+class TeamMember(BaseModel):
+    id: int
+    name: str
+    role: str
+
+class ProjectDetails(BaseModel):
+    start_date: str
+    end_date: str
+    team_members: List[TeamMember]
+
+class Project(BaseModel):
     project_id: int
+    name: str
+    details: ProjectDetails
+
+class Contact(BaseModel):
+    email: str
+    phone: str
+
+class Employee(BaseModel):
+    id: int
+    name: str
+    contact: Contact
+    projects: List[Project]
 
 class UpdateUser(BaseModel):
-    username: Optional[str] = None
-    email: Optional[EmailStr] = None
-    project_id: Optional[int] = None
+    name: Optional[str] = None
+    contact: Optional[Contact] = None
+    projects: Optional[List[Project]] = None
